713bd003607dc5917d9ad830df9be1c703318cdd
chungnantsai <s4310036@gmail.com>
2022-11-23 22:26:02 +0900

    add Navbar

--- yelp/client/my-app/src/routes/Home.jsx
+++ yelp/client/my-app/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AddRestaurant from "../components/AddRestaurant";
 import Header from "../components/Header"
+import Navbar from "../components/Navbar";
 import RestaurantList from "../components/RestaurantList";
 
 
 const Home = () => {
     return(
         <div>
+            <Navbar />
             <Header />
             <AddRestaurant />
             <RestaurantList />
